c512fe23b6391a09ae922b89ba28c065046c3692
Henrique <henrique@xds.com.br>
2022-04-09 17:14:20 -0300

    Update lwjgl -> lwjgl3

--- desktop/src/com/mack/brasilbois/desktop/DesktopLauncher.kt
+++ desktop/src/com/mack/brasilbois/desktop/DesktopLauncher.kt
@@ -1,14 +1,15 @@
 package com.mack.brasilbois.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
 import com.mack.brasilbois.BrBoisMain
 
 fun main(args : Array<String>) {
 
-    val config = LwjglApplicationConfiguration()
-    config.width = 1280
-    config.height = 720
-    config.title = "Brasil Bois"
-    LwjglApplication(BrBoisMain(), config)
+    val config = Lwjgl3ApplicationConfiguration()
+    config.setForegroundFPS(60)
+    config.apply {
+        setTitle("Brasil Bois")
+    }
+    Lwjgl3Application(BrBoisMain(), config)
 }
--- desktop/src/com/mack/brasilbois/desktop/DesktopLauncherEnemy.java
+++ desktop/src/com/mack/brasilbois/desktop/DesktopLauncherEnemy.java
@@ -1,15 +1,13 @@
 package com.mack.brasilbois.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mack.brasilbois.BrBoisMain;
 
 public class DesktopLauncherEnemy {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width= BrBoisMain.Companion.getWIDTH();
-		config.height= BrBoisMain.Companion.getHEIGHT();
-		config.title = BrBoisMain.Companion.getTITLE();
-		new LwjglApplication(new BrBoisMain(), config);
-	}
+//	public static void main (String[] arg) {
+//		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//		config.width= BrBoisMain.Companion.getWIDTH();
+//		config.height= BrBoisMain.Companion.getHEIGHT();
+//		config.title = BrBoisMain.Companion.getTITLE();
+//		new LwjglApplication(new BrBoisMain(), config);
+//	}
 }
